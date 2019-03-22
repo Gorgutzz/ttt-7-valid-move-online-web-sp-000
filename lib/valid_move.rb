af880dca0cf8a438b005033f43dd5848d5b80829
carefree-closure-1550 <leo.kalshteyn@gmail.com>
2019-03-22 20:18:28 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken? == false
+  if index.between?(0, 8) && position_taken?(board, index) == false
     true
   else
     false
-  end
 
 end
 
